2348e2bad606f8658bfaf6429f93ff1bdb447566
aguiarHugo <aguiarhugofx@gmail.com>
2023-05-12 14:57:08 -0300

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
 function App() {
@@ -18,6 +19,14 @@ function App() {
     setCoordinates({ latitude, longitude });
   };
 
+  const openGoogleMaps = () => {
+    if (coordinates) {
+      const { latitude, longitude } = coordinates;
+      const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+      window.open(url, "_blank");
+    }
+  };
+
   return (
     <div>
       <button
@@ -28,13 +37,31 @@ function App() {
         Lat / Long
       </button>
 
-      {coordinates && (
-        <>
-          <h3>Suas coordenadas são:</h3>
-          <p><span>Latitude:</span> {coordinates.latitude}</p>
-          <p><span>Longitude:</span> {coordinates.longitude}</p>
-        </>
-      )}
+      <CSSTransition
+        in={coordinates !== null}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div className="coordinates">
+          {coordinates && (
+            <p>
+              Latitude: {coordinates.latitude}<br />
+              Longitude: {coordinates.longitude}
+            </p>
+          )}
+
+          {coordinates && (
+            <button
+              className='map-button'
+              type='button'
+              onClick={openGoogleMaps}
+            >
+              Abrir no Google Maps
+            </button>
+          )}
+        </div>
+      </CSSTransition>
     </div>
   );
 }
